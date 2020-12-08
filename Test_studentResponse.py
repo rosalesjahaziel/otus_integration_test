1cab51b4a1d9bbf298d37616aa76f70a2a3c2494
jahaziel rosales <rosales.jahaziel.a@gmail.com>
2020-12-07 21:36:21 -0700

    adding id class validation

--- Test_studentResponse.py
+++ Test_studentResponse.py
@@ -6,6 +6,7 @@ class TestStudentClassResponse(unittest.TestCase):
     url = "https://gist.githubusercontent.com/edotus/bd63eefb9b4b1eacb641811f9a1a780d/raw/60e04520584f7a436917b0d5be2b6c18f039fadb/students_classes.json" 
     json_response = {}
     students = None
+    classes_list = None
 
     def setUp(self):
         print("getting json response")   
@@ -15,7 +16,8 @@ class TestStudentClassResponse(unittest.TestCase):
 
         self.json_response = response.json()
         self.students = self.json_response.get("students", [])
-    
+        self.classes_list = self.json_response.get("classes", [])
+
     def test_response_validation(self):
         for idx, student in enumerate(self.students):
             if not student:
@@ -28,8 +30,11 @@ class TestStudentClassResponse(unittest.TestCase):
             print("validating last name format")
             self.assertTrue(NamesRegexValidation("last", student.get("last", "")).validate())
             classes = student.get("studentClasses", [])
-            print("validating grades format")
+            print("validating Classes")
             for student_class in classes:
+                print("validating id")
+                self.assertIn(str(student_class.get("id", 0)), self.classes_list)
+                print("validating grades format")
                 self.assertTrue(GradeRegexValidation("grade", str(student_class.get("grade", 0))).validate())
 
     def tearDown(self):
